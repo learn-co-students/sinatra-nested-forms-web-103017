8f0b7aaa4e1eeb0bff32bdd6aef3248ce0350858
github username <github email address>
2017-11-21 14:04:21 -0500

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,22 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      "Welcome to the Nested Forms Lab! let's navigate to the '/new'"
+      #redirect '/new'
+    end
+
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      #binding.pry
+      @pirate = params["pirate"]
+      @ship1 = @pirate["ships"][0]
+      @ship2 = @pirate["ships"][1]
+      erb :"pirates/show"
+    end
 
   end
 end
