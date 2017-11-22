e5bea2c756e3e6395de5565048b4e541d4bd20c5
github username <github email address>
2017-11-22 14:06:51 -0500

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,27 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      @pirate_name = params[:pirate][:name]
+      @pirate_weight = params[:pirate][:weight]
+      @pirate_height = params[:pirate][:height]
+      @ship_one_name = params[:pirate][:ships][0][:name]
+      @ship_one_type = params[:pirate][:ships][0][:type]
+      @ship_one_booty = params[:pirate][:ships][0][:booty]
+      @ship_two_name = params[:pirate][:ships][1][:name]
+      @ship_two_type = params[:pirate][:ships][1][:type]
+      @ship_two_booty = params[:pirate][:ships][1][:booty]
+      erb :"pirates/show"
+    end
+
 
   end
 end
